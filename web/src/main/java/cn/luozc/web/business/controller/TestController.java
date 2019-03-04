bad14ae1f2a35e0b0a173a1e710b419e88603b4c
Administrator <1079648153@qq.com>
2019-03-04 22:15:34 +0800

    流程测试

--- web/src/main/java/cn/luozc/web/business/controller/TestController.java
+++ web/src/main/java/cn/luozc/web/business/controller/TestController.java
@@ -1,8 +1,13 @@
 package cn.luozc.web.business.controller;
 
+import org.activiti.engine.FormService;
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.form.FormProperty;
+import org.activiti.engine.form.TaskFormData;
 import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
+import org.activiti.engine.repository.Model;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.task.TaskQuery;
@@ -23,20 +28,31 @@ public class TestController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private FormService formService;
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @RequestMapping("list")
+    public Object list(){
+        List<Model> list = repositoryService.createModelQuery().list();
+        System.err.println(list);
+        return list;
+    }
 
     @RequestMapping("/start")
     public void start(){
         System.out.println("method startActivityDemo begin....");
         Map<String,Object> map = new HashMap<String,Object>();
-        map.put("apply","12");
-        map.put("approve","lisi");
+        map.put("userId","12");
         //流程启动
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("leave",map);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("Vacation",map);
 
-        //List<Task> list = taskService.createTaskQuery().processInstanceId(processInstance.getProcessInstanceId()).list();
-        System.out.println("流程实例id"+processInstance.getId());//流程实例id
-        System.out.println("流程定义ID"+processInstance.getProcessDefinitionId());//流程定义ID
-        List<Task> list = taskService.createTaskQuery().taskCandidateOrAssigned("12").list();
+
+        List<Task> list = taskService.createTaskQuery().processInstanceId(processInstance.getProcessInstanceId()).list();
+//        System.out.println("流程实例id"+processInstance.getId());//流程实例id
+//        System.out.println("流程定义ID"+processInstance.getProcessDefinitionId());//流程定义ID
+//        List<Task> list = taskService.createTaskQuery().taskCandidateOrAssigned("12").list();
         if (list!=null&&list.size()>0) {
             for (Task task : list) {
                 System.out.println("任务ID"+task.getId());
@@ -46,7 +62,14 @@ public class TestController {
                 System.out.println("流程实例id:"+task.getProcessInstanceId());
                 System.out.println("执行对象id:"+task.getExecutionId());
                 System.out.println("流程定义id:"+task.getProcessDefinitionId());
-               // taskService.complete(task.getId(),map);
+                TaskFormData taskFormData = formService.getTaskFormData(task.getId());
+                List<FormProperty> formProperties = taskFormData.getFormProperties();
+                System.err.println();
+                for (FormProperty formProperty:formProperties) {
+                    String name = formProperty.getName();
+                    System.err.println(name);
+                }
+                // taskService.complete(task.getId(),map);
 
             }
         }
